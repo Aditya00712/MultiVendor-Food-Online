ada7384cf4fababe28ee8cdcb2dc19c58f6d1ff2
Aditya00712 <aa2167906@gmail.com>
2024-11-10 10:56:08 +0530

    Add UserProfile model and update admin registration; configure media settings

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import *
 from django.contrib.auth.admin import UserAdmin
 
 
@@ -11,3 +11,4 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = ()
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
