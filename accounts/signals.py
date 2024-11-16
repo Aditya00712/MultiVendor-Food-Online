29722e82fc02424ca1312e4b71c6ad1bda2f777a
Aditya00712 <aa2167906@gmail.com>
2024-11-16 16:52:33 +0530

    Add user registration form, view, and template; implement user profile handling

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,17 +7,15 @@ def post_save_create_profile_receiver(sender, instance, created,**kwargs):
     print(created)
     if created:
         UserProfile.objects.create(user=instance)
-        print('User Profile created!')
     else:
         try:
             profile = UserProfile.objects.get(user=instance)
             profile.save()
         except:
             #create the user profile if does not exist:
-            print('User Profile does not exist, creating one...')
-        print('User Profile is Updated!')
+            UserProfile.objects.create(user=instance)
 
 @receiver(pre_save, sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-    print(instance.username, 'this user is about to be saved!')
-post_save.connect(post_save_create_profile_receiver, sender=User)
+    pass
+# post_save.connect(post_save_create_profile_receiver, sender=User)
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import *
 
 def registerUser(request):
-    return HttpResponse('This is user registration Form')
+    if request.method == 'POST':
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user= form.save(commit=False)
+            user.role = User.CUSTOMER
+            form.save()
+            return redirect('registerUser')
+    else:
+        form = UserForm
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/registerUser.html', context)
