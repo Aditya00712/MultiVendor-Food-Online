6ff761f471519409ddc0a481bf6569b474e7ecc7
Aditya00712 <aa2167906@gmail.com>
2024-11-17 19:01:24 +0530

    Add user registration validation and success messages; enhance form error handling

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from . models import *
+from . models import User
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -7,3 +7,12 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError('Passwords do not match')
+        return cleaned_data
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import *
+from .models import User
+from django.contrib import messages
 
 def registerUser(request):
     if request.method == 'POST':
-        print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
-            user= form.save(commit=False)
+            #Creating the user using the form
+            # password = form.cleaned_data['password']
+            # user= form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # form.save()
+
+            #Create the User using the create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
-            form.save()
+            user.save()
+            messages.success(request,'Your account has been created successfully')
             return redirect('registerUser')
+        else:
+            print('Invalid form')
+            print(form.errors)
     else:
         form = UserForm
     context = {
